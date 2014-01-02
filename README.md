921fff9d16bea7451403f9d144e976ac07b380f1
Brendan Bates <batesbrendan@gmail.com>
2014-01-02 17:02:51 +1100

    Allow for multiple clusters in same container. Ensure when-leader can be executed multiple times within same container

--- README.md
+++ README.md
@@ -30,17 +30,23 @@ It's pretty easy.
  (when-leader "my-cluster"
  	(println "I am leader! Feel my wrath!")
  	(initialise-something-that-can-only-run-on-one-node))
+
+;; => If the node is currently the leader, when-leader will return the result of executing the body.
+;; => If the node is NOT currently the leader, when-leader will return a future that will be pending until/if the node becomes leader of the cluster.
 ```
 
 The leader will be the leader of the cluster for it's JVM's entire life. When the host JVM shuts down, then another node
 in the cluster will take over.
+
+when-leader can be executed multiple times within an application for the same cluster, and all bodys will be run when the node becomes leader.
+
+An application node can attempt to become leader in multiple clusters, differentiated using the cluster name.
 
 ## What?
 Leader-gurantee (LG) is just a simple wrapper around [JGroups'](http://jgroups.org) [LockService](http://www.jgroups.org/javadoc/org/jgroups/blocks/locking/LockService.html).
 
 + LG uses UDP as its transport protocol - it uses the default UDP config, plus:
 + LG uses a Central controlled lock - see [CENTRAL_LOCK](http://www.jgroups.org/javadoc/org/jgroups/protocols/CENTRAL_LOCK.html)
-
 
 
 ## License
--- src/leader_guarantee/core.clj
+++ src/leader_guarantee/core.clj
@@ -4,30 +4,55 @@
 					 (org.jgroups JChannel)
 					 (java.util.concurrent.locks Lock)))
 
+(def get-lock-threads (atom {}))
+
 (defn- get-lock
-	[lock-service with-lock-fn]
+	[lock-service lock-promise]
 	(fn []
 		(let [lock (.getLock lock-service "leader")]
 			(do
 				(.lock lock)
-				(with-lock-fn)))))
+				(deliver lock-promise :is-leader)))))
+
+(defn- leader-thread
+	[cluster-name]
+	(locking get-lock-threads
+		(if-not (get @get-lock-threads cluster-name)
+			(let [channel (doto (JChannel.) ;; Uses udp by default
+											(.. getProtocolStack (addProtocol (doto (CENTRAL_LOCK.) (.init))))
+											(.connect cluster-name))
+						lock-service (LockService. channel)
+						lock-promise (promise)]
+				(get (swap! get-lock-threads
+							 assoc
+							 cluster-name
+							 {:thread
+								(doto (Thread. (get-lock lock-service lock-promise) "LeaderAppointmentThread")
+									(.setDaemon true)
+									(.start))
+								:promise lock-promise})
+						 cluster-name))
+			(get @get-lock-threads cluster-name))))
 
 (defn when-leader*
-	"Executes with-lock-fn when the local application container becomes leader of the cluster-name cluster."
-	[cluster-name with-lock-fn]
-	(let [channel (doto (JChannel.) ;; Uses udp by default
-									(.. getProtocolStack (addProtocol (doto (CENTRAL_LOCK.) (.init))))
-									(.connect cluster-name))
-				lock-service (LockService. channel)]
-		(doto (Thread. (get-lock lock-service with-lock-fn) "LeaderAppointmentThread")
-			(.setDaemon true)
-			(.start))))
+	"Executes f when the local application container becomes leader of the cluster-name cluster."
+	[cluster-name f]
+	(let [leader-promise (:promise (leader-thread cluster-name))]
+		(if (realized? leader-promise)
+			(f)
+			(future
+				(when (deref leader-promise) (f))))))
 
 (defmacro when-leader
-  "Execute body forms IFF the local application container has been appointed as the leader
+  "Execute body forms WHEN the local application container has been appointed as the leader
 	of the cluster named in cluster-name.
+	Can be executed multiple times within a container, and when the node becomes leader, will
+	execute each body.
 	The leader will stay leader for the entire lifetime of the JVM - only releasing control
-	when the JVM is shut down."
+	when the JVM is shut down.
+	Returns the result of executing body, OR a future that will execute body when the node becomes leader of the cluster.
+
+		Note: if the leader isn't the cluster leader when first called, the body will be executed in a future."
   [cluster-name & body]
-	`(letfn [(on-leader# [] (do ~@body))]
-		(when-leader* ~cluster-name on-leader#)))
+	`(letfn [(when-leader# [] (do ~@body))]
+		(when-leader* ~cluster-name when-leader#)))
